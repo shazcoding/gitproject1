ce40d7c20a774660e542b78e87eebe2abf79238a
shameel <shameelbca@gmail.com>
2026-03-31 21:46:45 +0530

    initiated

---
+++ src/com/shameel/Gitproject1/prog1.java
@@ -0,0 +1,10 @@
+package com.shameel.Gitproject1;
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hell king");
+	}
+
+}
